c5dea83e26fba20516d5816927fa8c1d5e71b177
Minora <MinoraTyan@yandex.tu>
2023-06-07 22:31:28 +0300

    DZ Task 4

---
+++ DZ_Task_004/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и 
+выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine()!);
+
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.WriteLine($"Число {max} является большим");
+
+
+/*if (num2 > max) max = num2;
+{
+Console.WriteLine($"Число {num2} является max");
+}
+if (num3 > max) max = num3;
+{
+Console.WriteLine($"Число {num3} является max");
+}
+else
+{
+    
+}*/

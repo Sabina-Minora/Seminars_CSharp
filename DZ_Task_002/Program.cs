4e288cd8e5f4504cf43e9c648798756dc9d3175c
Minora <MinoraTyan@yandex.tu>
2023-06-07 21:57:03 +0300

    DZ Task 2

---
+++ DZ_Task_002/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if(num1 > num2)
+{
+Console.WriteLine($"Число {num1} больше числа {num2}");
+}
+else
+{
+Console.WriteLine($"Число {num2} больше числа {num1}");
+}

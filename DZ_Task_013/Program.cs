6881dda9e02ef9bab3d9cc8ff7072dc8a9917348
Minora <MinoraTyan@yandex.tu>
2023-06-09 16:37:08 +0300

    DZ_Task_013

--- DZ_Task_013/Program.cs
+++ DZ_Task_013/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+ или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+
+Console.Clear();
+//Console.Write("Введите любое трёхзначное число: ");
+
+
+int ThirdDigit(int number)
+        {
+            int result = -1;
+            if (number >= 100)
+            {
+                while (number > 999)
+                {
+                    number = number / 10;
+                }
+                result = number % 10;
+            }
+            return result; 
+        }
+
+
+Console.Write("Введите любое трёхзначное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+ 
+if (ThirdDigit(number1) == -1)
+Console.WriteLine("Это число не является трёхзначным");
+else
+Console.WriteLine($"Третьей цифрой является: {ThirdDigit(number1)}");

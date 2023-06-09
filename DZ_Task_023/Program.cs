2bdaeb4879eb93ec4e9800d50604a5969b5f2569
Minora <MinoraTyan@yandex.tu>
2023-06-09 17:47:36 +0300

    DZ_Task_23

---
+++ DZ_Task_023/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+
+Console.Clear();
+Console.WriteLine("Введите любое число: ");
+double N = double.Parse(Console.ReadLine());
+double B = 1;
+
+Console.WriteLine($"Это таблица кубов от 1 до {N}");
+
+while (B <= N)
+{
+    double sqr = Math.Pow(B,3);
+    Console.WriteLine(sqr);
+    B++;
+}
